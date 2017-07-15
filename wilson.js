16eaa4562fbf60dba5c3ff55e85190b0af00851b
Michał Jezierski <ambinanitelo@gmail.com>
2017-07-15 22:25:22 +0200

    eslint fixes

--- wilson.js
+++ wilson.js
@@ -7,16 +7,16 @@ module.exports = function (positiveScore, total) {
         };
     }
 
-  // phat is the proportion of successes
-  // in a Bernoulli trial process
+    // phat is the proportion of successes
+    // in a Bernoulli trial process
     const phat = positiveScore / total;
 
-  // z is 1-alpha/2 percentile of a standard
-  // normal distribution for error alpha=5%
+    // z is 1-alpha/2 percentile of a standard
+    // normal distribution for error alpha=5%
     const z = 1.96;
 
-  // implement the algorithm
-  // (http://goo.gl/kgmV3g)
+    // implement the algorithm
+    // (http://goo.gl/kgmV3g)
     const a = phat + z * z / (2 * total);
     const b = z * Math.sqrt((phat * (1 - phat) + z * z / (4 * total)) / total);
     const c = 1 + z * z / total;
